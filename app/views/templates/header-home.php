690f97af058e1d1a540b62d7caa7ec3bddb9c8e4
cBrahmin <angad_dubey@hotmail.com>
2015-03-07 19:12:31 -0500

    minified vendor js

--- app/views/templates/header-home.php
+++ app/views/templates/header-home.php
@@ -19,9 +19,9 @@
     <!-- Vendor JS -->
     <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
     <script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
-    <script type="text/javascript" src="./public/js/vendor/typedjs/typed.js"></script>
-    <script type="text/javascript" src="./public/js/vendor/progressbar/progressbar.js"></script>
-    <script type="text/javascript" src="./public/js/vendor/smoothscroll/smoothscroll.js"></script>
+    <script type="text/javascript" src="./public/js/vendor/typedjs/typed.min.js"></script>
+    <script type="text/javascript" src="./public/js/vendor/progressbar/progressbar.min.js"></script>
+    <script type="text/javascript" src="./public/js/vendor/smoothscroll/smoothscroll.min.js"></script>
     <script type="text/javascript" src="./public/js/vendor/waypoints/jquery.waypoints.min.js"></script>
     <script type="text/javascript" src="./public/js/vendor/mmenu/mmenu.min.js"></script>
     <!-- CUSTOM JS -->
