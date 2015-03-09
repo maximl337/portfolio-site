d352a7b67f39f9a22eafc441532eaac4dc108ccc
cBrahmin <angad_dubey@hotmail.com>
2015-03-09 11:52:01 -0400

    moved JS to footer

--- app/views/templates/footer.php
+++ app/views/templates/footer.php
@@ -17,5 +17,16 @@
     </div>
 </footer>
 </div> <!-- /#body-wrap-->
+
+<!-- Vendor JS -->
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+    <script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="./public/js/vendor/typedjs/typed.min.js"></script>
+    <script type="text/javascript" src="./public/js/vendor/progressbar/progressbar.min.js"></script>
+    <script type="text/javascript" src="./public/js/vendor/smoothscroll/smoothscroll.min.js"></script>
+    <script type="text/javascript" src="./public/js/vendor/waypoints/jquery.waypoints.min.js"></script>
+    <script type="text/javascript" src="./public/js/vendor/mmenu/mmenu.min.js"></script>
+    <!-- CUSTOM JS -->
+    <script type="text/javascript" src="./public/js/main.js"></script>
 </body>
 </html>
