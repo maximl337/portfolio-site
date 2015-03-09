99f3fd6c13881e8f1027de320f544388d521d2ef
cBrahmin <angad_dubey@hotmail.com>
2015-03-09 10:40:16 -0400

    corrected social links

--- app/views/templates/footer.php
+++ app/views/templates/footer.php
@@ -8,7 +8,7 @@
                 <ul class="social-links">
                     <li><a title="Github Profile" href="https://github.com/maximl337"><i class="fa fa-github-square"></i></a></li>
                     <li><a title="Twitter profile" href="https://twitter.com/angaddby"><i class="fa fa-twitter-square"></i></a></li>
-                    <li><a title="Linkedin Profile" href="ca.linkedin.com/in/angaddubey"><i class="fa fa-linkedin-square"></i></a></li>
+                    <li><a title="Linkedin Profile" href="http://ca.linkedin.com/in/angaddubey"><i class="fa fa-linkedin-square"></i></a></li>
                     <li><a title="Facebook profile" href="https://www.facebook.com/angad.dubey.7"><i class="fa fa-facebook-square"></i></a></li>
                     <li><a title="Google plus profile" href="https://plus.google.com/101340480310801355869/about"><i class="fa fa-google-plus-square"></i></a></li>
                 </ul>
